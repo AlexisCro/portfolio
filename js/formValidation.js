eaf23f330b790683be72906960d8159995be4b4e
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2024-02-25 20:15:03 +0100

    form

---
+++ js/formValidation.js
@@ -0,0 +1,22 @@
+let form = document.getElementById('form-contact')
+let submitButton = document.getElementById('submitButton')
+let formElements = form.elements
+
+let requiredInputs = ["email", "message", "firstName", "lastName"]
+
+submitButton.addEventListener('click', function(event) {
+  let emptyFields = []
+
+  for(input in requiredInputs) {
+    if (formElements[requiredInputs[input]].value === "") {
+      formElements[requiredInputs[input]].placeholder = "This field is required"
+      formElements[requiredInputs[input]].style.border = "2px solid red"
+      emptyFields.push(requiredInputs[input].name)
+    }
+  }
+
+  if (emptyFields.length > 0) {
+    event.preventDefault()
+    form.reset()
+  }
+})
