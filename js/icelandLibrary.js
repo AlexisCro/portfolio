9f43381d09372cec31d7b95c751e80607b65b61e
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2024-02-28 17:05:32 +0100

    enhancement

--- js/icelandLibrary.js
+++ js/icelandLibrary.js
@@ -4,188 +4,188 @@ export let object = {
       'en': 'Vestrahorn Reflection',
       'fr': 'Reflet de Vestrahorn'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/vestrahorn_reflect.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/vestrahorn_reflect.jpg'
   },
   'aurora_landscape_jokullsarlon': {
     'name': {
       'en': 'Aurora above Jokullsarlon',
       'fr': 'Aurore boréale au-dessus de Jokullsarlon'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/aurora_landscape.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/aurora_landscape.jpg'
   },
   'hello_seal': {
     'name': {
       'en': 'Hello seal',
       'fr': 'Bonjour phoque'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/hello_seal.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/hello_seal.jpg'
   },
   'look_at_seal': {
     'name': {
       'en': 'Look at the seal',
       'fr': 'Regarder le phoque'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/look_seal.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/look_seal.jpg'
   },
   'seljalandsfoss': {
     'name': {
       'en': 'Seljalandsfoss',
       'fr': 'Seljalandsfoss'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/seljalandsfoss.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/seljalandsfoss.jpg'
   },
   'sky_jewels': {
     'name': {
       'en': 'Sky jewels',
       'fr': 'Bijoux du ciel'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/sky_jewels.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/sky_jewels.jpg'
   },
   'white_quiet': {
     'name': {
       'en': 'White calm',
       'fr': 'Calme blanc'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/white_quiet.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/white_quiet.jpg'
   },
   'sunset_jokullsarlon': {
     'name': {
       'en': 'Sunset on the Jokullsarlon',
       'fr': 'Coucher de soleil sur le Jokullsarlon'
     }, 
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/sunset_jokullsarlon.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/sunset_jokullsarlon.jpg'
   },
   'sunset_jewel': {
     'name': {
       'en': 'Sunset with Jokullsarlon jewels',
       'fr': 'Coucher de soleil avec les bijoux de Jokullsarlon'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/sunset_jewel.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/sunset_jewel.jpg'
   },
   'skogafoss_rainbow': {
     'name': {
       'en': 'Skogafoss Rainbow',
       'fr': 'Arc-en-ciel à Skogafoss'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/skogafoss_rainbow.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/skogafoss_rainbow.jpg'
   },
   'skogafoss_portrait': {
     'name': {
       'en': 'Autoportrait at Skogafoss',
       'fr': 'Autoportrait à Skogafoss'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/skogafoss_portrait.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/skogafoss_portrait.jpg'
   },
   'tumultuous_bruarfoss': {
     'name': {
       'en': 'Tumultuous Bruarfoss',
       'fr': 'Bruarfoss tumultueux'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/tumultuous_bruarfoss.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/tumultuous_bruarfoss.jpg'
   },
   'thingvellir_waterfall': {
     'name': {
       'en': 'Oxararfoss at Thingvellir',
       'fr': 'Oxararfoss à Thingvellir'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/thingvellir_waterfall.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/thingvellir_waterfall.jpg'
   },
   'the_flight_of_seagull': {
     'name': {
       'en': 'The flight of the seagull',
       'fr': 'Le vol de la mouette'
     }, 
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/the_flight_of_seagull.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/the_flight_of_seagull.jpg'
   },
   'take_rest': {
     'name': {
       'en': 'Take a rest',
       'fr': 'Prendre un repos'
     }, 
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/take_rest.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/take_rest.jpg'
   },
   'seagull_hunter': {
     'name': {
       'en': 'Seagull hunter',
       'fr': 'Chasseur de mouette'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/seagull_hunter.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/seagull_hunter.jpg'
   },
   'midfoss': {
     'name': {
       'en': 'Midfoss',
       'fr': 'Midfoss'
     }, 
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/midfoss.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/midfoss.jpg'
   },
   'godafoss': {
     'name': {
       'en': 'Godafoss',
       'fr': 'Godafoss'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/godafoss.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/godafoss.jpg'
   },
   'family_lagopede': {
     'name': {
       'en': 'Family of Lagopede',
       'fr': 'Famille de Lagopède'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/family_lagopede.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/family_lagopede.jpg'
   },
   'dynamic_diamon_beach': {
     'name': {
       'en': 'Iceberg on the Diamond Beach',
       'fr': 'Iceberg sur la plage de diamant'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/dynamic_diamon_beach.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/dynamic_diamon_beach.jpg'
   },
   'dominant_vestrahorn': {
     'name': {
       'en': 'Dominant Vestrahorn',
       'fr': 'Vestrahorn dominant',
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/dominant_vestrahorn.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/dominant_vestrahorn.jpg'
   },
   'calm_diamond_beach': {
     'name': {
       'en': 'Calm on the Diamond Beach',
       'fr': 'Calme sur la plage de diamant'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/calm_diamond_beach.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/calm_diamond_beach.jpg'
   },
   'bruarfoss': {
     'name': {
       'en': 'Bruarfoss',
       'fr': 'Bruarfoss',
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/bruarfoss.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/bruarfoss.jpg'
   },
   'aurora_jokullsarlon_portrait': {
     'name': {
       'en': 'Aurora at Jokullsarlon',
       'fr': 'Aurore boréale à Jokullsarlon'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/aurora_jokullsarlon_portrait.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/aurora_jokullsarlon_portrait.jpg'
   },
   'aurora_jokullsarlon_best': {
     'name': {
       'en': 'Reflect of aurora',
       'fr': 'Reflet d\'aurore',
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/aurora_jokullsarlon_best.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/aurora_jokullsarlon_best.jpg'
   },
   'small_jewels': {
     'name': {
       'en': 'Small jewels',
       'fr': 'Petits bijoux'
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/small_jewels.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/small_jewels.jpg'
   },
   'cloudy_mountains': {
     'name': {
       'en': 'Cloudy mountains',
       'fr': 'Montagnes nuageuses',
     },
-    'imageUrl': '../../../assets/img/portfolio_photo/iceland/cloudy_mountains.jpg'
+    'imageUrl': '/assets/img/portfolio_photo/iceland/cloudy_mountains.jpg'
   }
 }
